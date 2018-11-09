9ef5684837e6eb26d89acea3d1b6ee24c40bcb5c
Corey Emery <cemery@andrew.cmu.edu>
2018-11-09 00:12:44 -0500

    Top-Level Framework

--- wick-site/src/App.js
+++ wick-site/src/App.js
@@ -1,28 +1,35 @@
+/*
+    Corey Emery (cemery@andrew.cmu.edu)
+    WickEditor Website
+    App.js
+
+    Top-Level React Component
+*/
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        // Website current page
+        this.state = {
+            active: "Home"
+        };
+    }
+
+    handleClick = (page) => {this.setState({active: page});}
+
+    render() {
+        return(
+            <div className="App">
+                <Header onClick={this.handleClick} />
+                <Content active={this.state.active} />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
