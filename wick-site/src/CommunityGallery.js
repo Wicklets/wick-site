3219f85dc010bc33c63a9b9f5972a63834e1d5dc
Corey Emery <cemery@andrew.cmu.edu>
2018-11-24 12:50:29 -0500

    Community Page mostly responsive
    Talk to Luca and Zach about Image Resizing...probably after PUI

--- wick-site/src/CommunityGallery.js
+++ wick-site/src/CommunityGallery.js
@@ -17,113 +17,113 @@ import './CommunityGallery.scss';
 const CommunityGallery = () => (
     <Fragment>
         <Row>
-            <Col sm="4">
+            <Col xs="12" md="4">
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
                 </Row>
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                </Row>
-            </Col>
-            <Col sm="4">
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={booth} className="CommunityGallery-image" /></Col>
-                </Row>
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={booth} className="CommunityGallery-image" /></Col>
-                </Row>
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="12"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
                 </Row>
             </Col>
-            <Col sm="4">
+            <Col xs="12" md="4">
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12">
+                    <Col xs="12"><Img src={booth} className="CommunityGallery-image" /></Col>
+                </Row>
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="12"><Img src={booth} className="CommunityGallery-image" /></Col>
+                </Row>
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                </Row>
+            </Col>
+            <Col xs="12" md="4">
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="12">
                         <Img src={coreyemery} className="CommunityGallery-image" />
                     </Col>
                 </Row>
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
                 </Row>
             </Col>
         </Row>
         <Row>
-            <Col sm="4">
+            <Col xs="12" md="4">
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><div className="CommunityGallery-testText2"><div>something</div></div></Col>
+                    <Col xs="12"><div className="CommunityGallery-testText2"><div>something</div></div></Col>
                 </Row>
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
                 </Row>
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={booth} className="CommunityGallery-image" /></Col>
+                    <Col xs="12"><Img src={booth} className="CommunityGallery-image" /></Col>
                 </Row>
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                </Row>
-            </Col>
-            <Col sm="4">
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><div className="CommunityGallery-testText1"><div>Here are a few tutorials to help you get started with the Wick Editor. Have an idea for an example, or want to submit your.</div></div></Col>
-                </Row>
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                </Row>
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
                 </Row>
             </Col>
-            <Col sm="4">
+            <Col xs="12" md="4">
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6">
+                    <Col xs="12"><div className="CommunityGallery-testText1"><div>Here are a few tutorials to help you get started with the Wick Editor. Have an idea for an example, or want to submit your.</div></div></Col>
+                </Row>
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="12"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                </Row>
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                </Row>
+            </Col>
+            <Col xs="12" md="4">
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="6">
                         <Img src={coreyPGSS} className="CommunityGallery-image" />
                     </Col>
-                    <Col sm="6">
+                    <Col xs="6">
                         <Img src={coreyPGSS} className="CommunityGallery-image" />
                     </Col>
                 </Row>
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12">
+                    <Col xs="12">
                         <Img src={coreyemery} className="CommunityGallery-image" />
                     </Col>
                 </Row>
             </Col>
         </Row>
         <Row>
-            <Col sm="4">
+            <Col xs="12" md="4">
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="12"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
                 </Row>
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={booth} className="CommunityGallery-image" /></Col>
-                </Row>
-            </Col>
-            <Col sm="4">
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={booth} className="CommunityGallery-image" /></Col>
-                </Row>
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="12"><Img src={booth} className="CommunityGallery-image" /></Col>
-                </Row>
-                <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="12"><Img src={booth} className="CommunityGallery-image" /></Col>
                 </Row>
             </Col>
-            <Col sm="4">
+            <Col xs="12" md="4">
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="12"><Img src={booth} className="CommunityGallery-image" /></Col>
                 </Row>
                 <Row className="CommunityGallery-galleryRow">
-                    <Col sm="6"><Img src={coreyPGSS} className="CommunityGallery-image" /></Col>
-                    <Col sm="6"><Img src={coreyPGSS} className="CommunityGallery-image" /></Col>
+                    <Col xs="12"><Img src={booth} className="CommunityGallery-image" /></Col>
+                </Row>
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                </Row>
+            </Col>
+            <Col xs="12" md="4">
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyemery} className="CommunityGallery-image" /></Col>
+                </Row>
+                <Row className="CommunityGallery-galleryRow">
+                    <Col xs="6"><Img src={coreyPGSS} className="CommunityGallery-image" /></Col>
+                    <Col xs="6"><Img src={coreyPGSS} className="CommunityGallery-image" /></Col>
                 </Row>
             </Col>
         </Row>
