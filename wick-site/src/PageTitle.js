331989466f7a0498c3e3de6ace07214dc91b273e
Corey Emery <cemery@andrew.cmu.edu>
2018-11-27 00:24:11 -0500

    Revert "Infinite Scroll header"
    This reverts commit 78d0408c471bd8d572a2ca755a1214df5bae66a1.

--- wick-site/src/PageTitle.js
+++ wick-site/src/PageTitle.js
@@ -8,13 +8,16 @@
 
 import React from 'react';
 import {Container, Row, Col} from 'reactstrap';
+import Img from 'react-image';
 import {Button} from 'reactstrap';
-import WickWorldContainer from './WickWorldContainer.js';
+import wickworld from './wickworld.svg';
 import './PageTitle.scss';
 
 const PageTitle = ({title, showButton, showSubtitle, buttonLabel, onClick, children}) => (
     <Container>
-        <WickWorldContainer />
+        <div className="PageTitle-wickWorld">
+            <Img src={wickworld} className="PageTitle-wickWorld" />
+        </div>
         <Row id="PageTitle-titleRow" className="PageTitle-row">{title}</Row>
         <Row id="PageTitle-buttonRow" className="PageTitle-row">
             <Col xs="7" sm="5" md="4" lg="3" xl="2">
