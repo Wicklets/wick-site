5aeecb4806d432a2adebf629223bc763a298292c
Corey Emery <cemery@andrew.cmu.edu>
2019-01-04 16:15:07 -0500

    Basic React Router

--- src/NavBar/Header.js
+++ src/NavBar/Header.js
@@ -7,6 +7,7 @@
 */
 
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import {
   Collapse,
@@ -18,75 +19,105 @@ import {
   NavLink
 } from 'reactstrap';
 
+import HomePageContainer from '../HomePage/HomePageContainer.js';
+import AboutPageContainer from '../AboutPage/AboutPageContainer.js';
+import LearnPageContainer from '../LearnPage/LearnPageContainer.js';
+import CommunityPageContainer from '../CommunityPage/CommunityPageContainer.js'
+
 import '../scss_styles/Header.scss';
 
+const home = () => (<HomePageContainer />)
+const about = () => (<AboutPageContainer />)
+const learn = () => (<LearnPageContainer />)
+const community = () => (<CommunityPageContainer />)
+
+
 const Header = ({page, onClickChangePage, onClickToggleDropdown, onClickLaunchEditor, backgroundAnimationClass, isDropdownOpen}) => (
-    <div id="Header-container">
-        <Navbar light expand="sm">
-            <div className={backgroundAnimationClass} />
-            <NavbarBrand
-                href="#"
-                id={(page === "home" ? "Header-activeItem" : "")}
-                className="Header-brand--narrow"
-                onClick={() => onClickChangePage("home")}
-            >
-                wick editor
-            </NavbarBrand>
-            <NavbarToggler onClick={onClickToggleDropdown} />
-            <Collapse isOpen={isDropdownOpen} navbar>
-                <Nav className="ml-auto mr-auto" navbar>
-                    <NavbarBrand
-                        href="#"
-                        id={(page === "home" ? "Header-activeItem" : "")}
-                        className="Header-brand--wide"
-                        onClick={() => onClickChangePage("home")}
-                    >
-                        wick editor
-                    </NavbarBrand>
-                    <NavItem>
-                        <NavLink
-                            href="#"
-                            id={(page === "create" ? "Header-activeItem" : "")}
-                            className="Header-navItem"
-                            onClick={onClickLaunchEditor}
+    <Router>
+        <div>
+        <div id="Header-container">
+            <Navbar light expand="sm">
+                <div className={backgroundAnimationClass} />
+                <NavbarBrand
+                    tag={Link}
+                    to="/"
+                    id={(page === "home" ? "Header-activeItem" : "")}
+                    className="Header-brand--narrow"
+                    onClick={() => onClickChangePage("home")}
+                >
+                    wick editor
+                </NavbarBrand>
+                <NavbarToggler onClick={onClickToggleDropdown} />
+                <Collapse isOpen={isDropdownOpen} navbar>
+                    <Nav className="ml-auto mr-auto" navbar>
+
+                        <NavbarBrand
+                            tag={Link}
+                            to="/"
+                            id={(page === "home" ? "Header-activeItem" : "")}
+                            className="Header-brand--wide"
+                            onClick={() => onClickChangePage("home")}
                         >
-                            create
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            href="#"
-                            id={(page === "learn" ? "Header-activeItem" : "")}
-                            className="Header-navItem"
-                            onClick={() => onClickChangePage("learn")}
-                        >
-                            learn
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            href="#"
-                            id={(page === "about" ? "Header-activeItem" : "")}
-                            className="Header-navItem"
-                            onClick={() => onClickChangePage("about")}
-                        >
-                            about
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            href="#"
-                            id={(page === "community" ? "Header-activeItem" : "")}
-                            className="Header-navItem"
-                            onClick={() => onClickChangePage("community")}
-                        >
-                            community
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
-    </div>
+                            wick editor
+                        </NavbarBrand>
+
+                        <NavItem>
+                            <NavLink
+                                href="#"
+                                id={(page === "create" ? "Header-activeItem" : "")}
+                                className="Header-navItem"
+                                onClick={onClickLaunchEditor}
+                            >
+                                create
+                            </NavLink>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLink
+                                tag={Link}
+                                to="/learn/"
+                                id={(page === "learn" ? "Header-activeItem" : "")}
+                                className="Header-navItem"
+                                onClick={() => onClickChangePage("learn")}
+                            >
+                                learn
+                            </NavLink>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLink
+                                tag={Link}
+                                to="/about/"
+                                id={(page === "about" ? "Header-activeItem" : "")}
+                                className="Header-navItem"
+                                onClick={() => onClickChangePage("about")}
+                            >
+                                about
+                            </NavLink>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLink
+                                tag={Link}
+                                to="/community/"
+                                id={(page === "community" ? "Header-activeItem" : "")}
+                                className="Header-navItem"
+                                onClick={() => onClickChangePage("community")}
+                            >
+                                community
+                            </NavLink>
+                        </NavItem>
+
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+        <Route path="/" exact component={home} />
+        <Route path="/about/" component={about} />
+        <Route path="/learn/" component={learn} />
+        <Route path="/community/" component={community} />
+        </div>
+    </Router>
 );
 
 export default Header;
--- src/Wick.js
+++ src/Wick.js
@@ -1,22 +1,19 @@
 /*
     Corey Emery (cemery@andrew.cmu.edu)
     WickEditor Website
-    App.js
+    Wick.js
 
     Top-Level React Component
 */
 
 import React, {Component} from 'react';
 
-import Content from './Content.js';
 import HeaderContainer from './NavBar/HeaderContainer.js';
 import Footer from './Footer/Footer.js';
-import PageTitleContainer from './ContentComponents/PageTitle/PageTitleContainer.js';
 
 import './scss_styles/Wick.scss';
-import './scss_styles/App.scss';
 
-class App extends Component {
+class Wick extends Component {
     constructor(props) {
         super(props);
 
@@ -30,19 +27,15 @@ class App extends Component {
 
     render() {
         return(
-            <div className="App">
+            <div className="Wick">
                 <HeaderContainer
                     page={this.state.page}
                     onClickChangePage={this.handleClick}
                 />
-                <PageTitleContainer
-                    page={this.state.page}
-                />
-                <Content page={this.state.page} />
                 <Footer />
             </div>
         );
     }
 };
 
-export default App;
+export default Wick;
