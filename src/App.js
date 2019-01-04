af085cffc6ff167706183e8803be3feaaa14b126
Corey Emery <cemery@andrew.cmu.edu>
2019-01-04 15:34:50 -0500

    Revert "temp"
    This reverts commit ce8f8e680453005603ddba85cec30d60c5dc52f4.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 /*
     Corey Emery (cemery@andrew.cmu.edu)
     WickEditor Website
-    Wick.js
+    App.js
 
     Top-Level React Component
 */
 
 import React, {Component} from 'react';
 
+import Content from './Content.js';
 import HeaderContainer from './NavBar/HeaderContainer.js';
 import Footer from './Footer/Footer.js';
+import PageTitleContainer from './ContentComponents/PageTitle/PageTitleContainer.js';
 
 import './scss_styles/Wick.scss';
+import './scss_styles/App.scss';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
 
+        // Website current page
+        this.state = {
+            page: "home"
+        };
+    }
+
+    handleClick = (page) => {this.setState({page})}
 
     render() {
         return(
-            <div>
-            <div className="Wick">
+            <div className="App">
                 <HeaderContainer
                     page={this.state.page}
                     onClickChangePage={this.handleClick}
                 />
+                <PageTitleContainer
+                    page={this.state.page}
+                />
+                <Content page={this.state.page} />
                 <Footer />
-            </div>
             </div>
         );
     }
