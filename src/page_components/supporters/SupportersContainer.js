66d210a69636e683b4c99f7d9e342fc72bbb3703
Corey Emery <cemery@andrew.cmu.edu>
2019-01-17 16:09:31 -0500

    Collage Improved

--- src/page_components/supporters/SupportersContainer.js
+++ src/page_components/supporters/SupportersContainer.js
@@ -18,46 +18,46 @@ class SupportersContainer extends Component {
 
         this.state = {
             top: [
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery}
+                {name: "Name McName1", img: coreyemery},
+                {name: "Name McName2", img: coreyemery},
+                {name: "Name McName3", img: coreyemery},
+                {name: "Name McName4", img: coreyemery},
+                {name: "Name McName5", img: coreyemery},
+                {name: "Name McName6", img: coreyemery},
+                {name: "Name McName7", img: coreyemery},
+                {name: "Name McName8", img: coreyemery},
+                {name: "Name McName9", img: coreyemery},
+                {name: "Name McName0", img: coreyemery}
             ],
             topMiddle: [
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery}
+                {name: "Name McName11", img: coreyemery},
+                {name: "Name McName12", img: coreyemery},
+                {name: "Name McName13", img: coreyemery},
+                {name: "Name McName14", img: coreyemery},
+                {name: "Name McName15", img: coreyemery},
+                {name: "Name McName16", img: coreyemery},
+                {name: "Name McName17", img: coreyemery},
+                {name: "Name McName18", img: coreyemery},
+                {name: "Name McName19", img: coreyemery},
+                {name: "Name McName20", img: coreyemery},
+                {name: "Name McName10", img: coreyemery}
             ],
             lowMiddle: [
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
+                {name: "Name McName21", img: coreyemery},
+                {name: "Name McName22", img: coreyemery},
+                {name: "Name McName23", img: coreyemery},
+                {name: "Name McName24", img: coreyemery},
+                {name: "Name McName25", img: coreyemery},
+                {name: "Name McName26", img: coreyemery},
+                {name: "Name McName27", img: coreyemery},
+                {name: "Name McName28", img: coreyemery},
             ],
             low: [
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery},
-                {name: "Name McName", img: coreyemery}
+                {name: "Name McName31", img: coreyemery},
+                {name: "Name McName32", img: coreyemery},
+                {name: "Name McName33", img: coreyemery},
+                {name: "Name McName34", img: coreyemery},
+                {name: "Name McName35", img: coreyemery}
             ]
         };
     }
