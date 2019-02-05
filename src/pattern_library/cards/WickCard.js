6c1fe1905c18751d05409b174d0f0eb16292ac1e
Corey Emery <cemery@andrew.cmu.edu>
2019-02-05 15:46:41 -0500

    Actually that wasn't that bad

--- src/pattern_library/cards/WickCard.js
+++ src/pattern_library/cards/WickCard.js
@@ -8,12 +8,12 @@
 
 import React, { Fragment } from 'react';
 import Img from 'react-image';
-import ResponsiveEmbed from 'react-responsive-embed';
 
 import { Card, CardImg, CardText, CardTitle, CardImgOverlay } from 'reactstrap';
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 import arrow from '../../Images/Tutorials/arrow.svg';
+
+import WickCardModal from './WickCardModal.js';
 
 import '../../scss_styles/WickCard.scss';
 
@@ -32,22 +32,7 @@ const WickCard = ({title, body, image, onClick, includeModal, modalOpen, onMouse
             </CardImgOverlay>
         </Card>
         {includeModal && (
-            <Modal
-                className="WickCard-modal"
-                isOpen={modalOpen}
-                toggle={onClick}
-                size="lg"
-            >
-                <ModalHeader className="WickCard-modalHeader">
-                    <div className="WickCard-modalNumber">Tutorial #</div>
-                    <div className="WickCard-modalUnderline" />
-                    <div className="WickCard-modalTitle">{title}</div>
-                </ModalHeader>
-                <ModalBody>
-                    <ResponsiveEmbed src="https://www.youtube.com/embed/IpF8fMXN6To" ratio="16:9" />
-                </ModalBody>
-                <ModalFooter />
-            </Modal>
+            <WickCardModal title={title} onClick={onClick} modalOpen={modalOpen} />
         )}
     </Fragment>
 );
---
+++ src/pattern_library/cards/WickCardModal.js
@@ -0,0 +1,36 @@
+/*
+    Corey Emery (cemery@andrew.cmu.edu)
+    WickEditor Website
+    WickCardModal.js
+
+    Component for Card Modals
+*/
+
+import React from 'react';
+import ResponsiveEmbed from 'react-responsive-embed';
+
+import { Card, CardImg, CardText, CardTitle, CardImgOverlay } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+import '../../scss_styles/WickCardModal.scss';
+
+const WickCardModal = ({title, onClick, modalOpen}) => (
+    <Modal
+        className="WickCardModal-container"
+        isOpen={modalOpen}
+        toggle={onClick}
+        size="lg"
+    >
+        <ModalHeader className="WickCardModal-header">
+            <div className="WickCardModal-number">Tutorial #</div>
+            <div className="WickCardModal-underline" />
+            <div className="WickCardModal-title">{title}</div>
+        </ModalHeader>
+        <ModalBody>
+            <ResponsiveEmbed src="https://www.youtube.com/embed/IpF8fMXN6To" ratio="16:9" />
+        </ModalBody>
+        <ModalFooter />
+    </Modal>
+);
+
+export default WickCardModal;
