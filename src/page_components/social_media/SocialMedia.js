0b0d1191bf76c25834456ccf8d232682500b9528
Zach Rispoli <zach.rispoli@gmail.com>
2019-06-17 02:32:40 -0400

    update instagram link

--- src/page_components/social_media/SocialMedia.js
+++ src/page_components/social_media/SocialMedia.js
@@ -27,7 +27,7 @@ const SocialMedia = () => (
     <div className="SocialMedia-container">
         <SocialMediaIcon image={facebook} highlight={facebookdark} link="https://www.facebook.com/wickeditor/" />
         <SocialMediaIcon image={twitter} highlight={twitterdark} link="https://twitter.com/wickeditor" />
-        <SocialMediaIcon image={instagram} highlight={instagramdark} link="https://www.instagram.com/" />
+        <SocialMediaIcon image={instagram} highlight={instagramdark} link="https://www.instagram.com/wickeditor" />
         <SocialMediaIcon image={youtube} highlight={youtubedark} link="https://www.youtube.com/channel/UCXUM4laL0jXCO4wJjY15xqg" />
         <SocialMediaIcon image={patreon} highlight={patreondark} link="https://www.patreon.com/WickEditor" />
     </div>
