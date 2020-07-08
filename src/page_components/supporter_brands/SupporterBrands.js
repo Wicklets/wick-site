65d75a6662e454c9c0a7eaa14ed3f44c09d0346b
Luca Damasco <luca.damasco@gmail.com>
2020-07-08 11:18:00 -0400

    Add terms + COSA

--- src/page_components/supporter_brands/SupporterBrands.js
+++ src/page_components/supporter_brands/SupporterBrands.js
@@ -16,6 +16,7 @@ import studio from '../../Images/Supporters/studio.svg';
 import henryarmero from '../../Images/Supporters/henryarmero.svg';
 import olympus from '../../Images/Supporters/olympus.svg';
 import bxa from '../../Images/Supporters/bxa.svg';
+import cosa from '../../Images/Supporters/cosa_logo.png';
 
 import '../../scss_styles/SupporterBrands.scss';
 
@@ -23,14 +24,18 @@ const SupporterBrands = () => (
     <Row className="Wick-row">
         <Col xs="10" md="8" lg="6" xl="5">
             <div className="SupporterBrands-row">
-                <Img src={mozilla} />
-                <Img src={studio} />
+                <Img className="supporter-brand large" src={mozilla} />
+                <Img className="supporter-brand large" src={studio} />
             </div>
             <div className="SupporterBrands-spacer" />
             <div className="SupporterBrands-row">
-                <Img src={henryarmero} />
-                <Img src={olympus} />
-                <Img src={bxa} />
+                <Img className="supporter-brand medium" src={cosa} />
+            </div>
+            <div className="SupporterBrands-spacer" />
+            <div className="SupporterBrands-row">
+                <Img className="supporter-brand small" src={henryarmero} />
+                <Img className="supporter-brand small" src={olympus} />
+                <Img className="supporter-brand small" src={bxa} />
             </div>
         </Col>
     </Row>
