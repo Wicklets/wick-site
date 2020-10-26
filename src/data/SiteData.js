0a52bf96b403c743043318ff35842d9cec5cea0d
Luca Damasco <luca.damasco@gmail.com>
2020-10-26 13:01:35 -0400

    Update site with new patreon and gthub numbers.

--- src/data/SiteData.js
+++ src/data/SiteData.js
@@ -47,9 +47,9 @@ import img_hermanos from '../Images/Community/hermanos.png';
 import img_theInspirator from '../Images/Community/theInspirator.png';
 
 // Support Us progress bar and goals
-export const supportData = {patreonProgress: 224,
+export const supportData = {patreonProgress: 214,
     patreonGoal: 1000, 
-    githubProgress: 1, 
+    githubProgress: 2, 
     githubGoal: 10}; 
 
 export const examples = [
